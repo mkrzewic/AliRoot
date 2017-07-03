74e56f135af874650a555288df33479fdbf7585f
drohr <drohr@cern.ch>
2017-07-03 18:54:10 +0200

    fix querrying of HLT operation mode

--- TPC/TPCbase/AliTPCcalibDB.cxx
+++ TPC/TPCbase/AliTPCcalibDB.cxx
@@ -1988,8 +1988,7 @@ void AliTPCcalibDB::UpdateChamberHighVoltageData()
   // check active state by analysing the scalers
   //
   // initialise graph with active running
-  const char* hltMode = NULL;
-  hltMode = gSystem->Getenv("HLT_ONLINE_MODE");
+  bool hltMode = getenv("HLT_ONLINE_MODE") && strcmp(getenv("HLT_ONLINE_MODE"), "on") == 0;
 
   AliCDBEntry *entry = NULL;
   if (!hltMode) entry = GetCDBEntry("GRP/CTP/Scalers");
